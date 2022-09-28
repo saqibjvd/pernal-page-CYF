0dfde53faec84cf7dd59f7b9ebeddf23fb2212e9
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2022-09-28 20:37:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -225,18 +225,6 @@ function getNumberOrString(value) {
 }
 
 
-// function getNumberOrString(value) {
-//   // Convert a string value to a number if possible
-//   let number_value = Number(value);
-//   if (Number.isNaN(number_value)) {
-//     return value
-//   } else {
-//     return number_value
-//   }
-// }
-
-
-
 document.getElementById('send').addEventListener('click', (event) => {
   let element_name = document.getElementById('name');
   let new_li = document.createElement('li');
